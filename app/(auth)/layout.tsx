e826a5cdeccb3bdcbd92e0c6943b9a359cb61085
Agustina Chaer <aguschaer@gmail.com>
2023-05-06 09:19:25 -0300

    add auth layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="flex items-start min-h-screen bg-cover bg-auth">
+      <div className="mt-16 bg-white ml-28 px-9 py-11 min-w-[360px] rounded-lg">
+        <Image
+          src="/logo-with-text.svg"
+          alt="Black Market"
+          width={173}
+          height={30}
+          className="mx-auto mb-12"
+        />
+        {children}
+      </div>
+    </main>
+  );
+}
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUpPage() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      This is the login page
+    </main>
+  );
+}
