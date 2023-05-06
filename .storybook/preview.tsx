693ae2760d5b4ca2abb74e90dc0620551e5ab2b1
Agustina Chaer <aguschaer@gmail.com>
2023-05-06 11:27:57 -0300

    config dm sans font for app and storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { Preview } from "@storybook/react";
+import { DM_Sans } from "next/font/google";
 import "../src/app/globals.css";
 
 import * as nextImage from "next/image";
@@ -9,7 +10,16 @@ Object.defineProperty(nextImage, "default", {
   value: (props) => <img {...props} />,
 });
 
+const dmSans = DM_Sans({ weight: "500", subsets: ["latin"] });
+
 const preview: Preview = {
+  decorators: [
+    (StoryFn) => (
+      <div className={dmSans.className}>
+        <StoryFn />
+      </div>
+    ),
+  ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const dmSans = DM_Sans({ weight: "500" });
 
 export const metadata = {
   title: "Create Next App",
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={dmSans.className}>{children}</body>
     </html>
   );
 }
